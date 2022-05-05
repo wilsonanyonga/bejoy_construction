11298b4d5cb7655d1d3308aa3c44c63160062768
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-05-05 21:12:21 +0300

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -53,43 +53,84 @@ class Home extends StatelessWidget {
           // ),
         ),
         const SizedBox(height: 20),
-        const TextField(
-          decoration: InputDecoration(
-            filled: true,
-          ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            for (var i = 0; i < 3; i++)
+              Container(
+                width: 100,
+                height: 100,
+                // color: Colors.red,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.red,
+                ),
+                child: Center(
+                  child: Column(
+                    children: const [
+                      SizedBox(height: 15),
+                      Text(
+                        '1',
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'Cement',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+          ],
         ),
         const SizedBox(height: 20),
-        Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            for (var i = 0; i < 3; i++)
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.red,
+                ),
+                // color: Colors.red,
+                child: Center(
+                  child: Column(
+                    children: const [
+                      SizedBox(height: 15),
+                      Text(
+                        '2500',
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'Gypsum Boards  (1kg)',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              Text(
-                '$counter',
-                // 'something',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ],
-          ),
+          ],
         ),
+        const SizedBox(height: 20),
       ],
     );
   }

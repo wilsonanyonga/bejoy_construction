a1f95440402919f2401704d85f2ab8dc6d65acb2
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-05-06 11:52:32 +0300

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,134 +9,96 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(height: 20),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(40.0, 0.0, 40.0, 0.0),
-          child: TextField(
-            decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(5),
-                borderSide: BorderSide.none,
-                // borderSide: const BorderSide(
-                //   color: Colors.green,
-                //   width: 1.0,
-                // ),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: const BorderSide(
-                  color: Colors.blue,
-                  width: 1.0,
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(40.0, 0.0, 40.0, 0.0),
+            child: TextField(
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide: BorderSide.none,
+                  // borderSide: const BorderSide(
+                  //   color: Colors.green,
+                  //   width: 1.0,
+                  // ),
                 ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(
+                    color: Colors.blue,
+                    width: 1.0,
+                  ),
+                ),
+                filled: true,
+                fillColor: Colors.blue.shade100,
+                // input border should appear when data is being modified
+                // border: OutlineInputBorder(
+                //   borderRadius: BorderRadius.circular(10.0),
+                // ),
+                labelText: 'Search',
+                prefixIcon: const Icon(Icons.search),
               ),
-              filled: true,
-              fillColor: Colors.blue.shade100,
-              // input border should appear when data is being modified
-              // border: OutlineInputBorder(
-              //   borderRadius: BorderRadius.circular(10.0),
-              // ),
-              labelText: 'Search',
-              prefixIcon: const Icon(Icons.search),
+            ),
+
+            // child: TextField(
+            //   decoration: InputDecoration(
+            //     labelText: 'Gender',
+            //     fillColor: Colors.blue.shade100,
+            //     border: OutlineInputBorder(
+            //       borderRadius: BorderRadius.circular(30),
+            //     ),
+            //   ),
+            // ),
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            'Project Name: GLUK',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
             ),
           ),
-
-          // child: TextField(
-          //   decoration: InputDecoration(
-          //     labelText: 'Gender',
-          //     fillColor: Colors.blue.shade100,
-          //     border: OutlineInputBorder(
-          //       borderRadius: BorderRadius.circular(30),
-          //     ),
-          //   ),
-          // ),
-        ),
-        const SizedBox(height: 20),
-        const Text(
-          'Project Name: GLUK',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
+          const SizedBox(height: 20),
+          const Text(
+            'Project Cost: \$1,000,000',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
           ),
-        ),
-        const SizedBox(height: 20),
-        const Text(
-          'Project Cost: \$1,000,000',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
+          const SizedBox(height: 30),
+          const Text(
+            'Materials Used',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
           ),
-        ),
-        const SizedBox(height: 20),
-        const Text(
-          'Materials Used',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-        ),
-        const SizedBox(height: 20),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            for (var i = 0; i < 3; i++)
-              Container(
-                width: 100,
-                // height: 100,
-                // color: Colors.red,
-                padding: const EdgeInsets.all(5),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.red,
-                ),
-                child: Column(
-                  children: const [
-                    SizedBox(height: 15),
-                    Text(
-                      '1',
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                    Text(
-                      'Cement',
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-          ],
-        ),
-        const SizedBox(height: 20),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            for (var i = 0; i < 3; i++)
-              Container(
-                width: 100,
-                // height: ,
-                padding: const EdgeInsets.all(5),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.red,
-                ),
-                // color: Colors.red,
-                child: SingleChildScrollView(
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              for (var i = 0; i < 3; i++)
+                Container(
+                  width: 100,
+                  // height: 100,
+                  // color: Colors.red,
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.red[400],
+                  ),
                   child: Column(
                     children: const [
                       SizedBox(height: 15),
                       Text(
-                        '2500',
+                        '1',
                         style: TextStyle(
                           fontSize: 40,
                           fontWeight: FontWeight.bold,
@@ -144,23 +106,63 @@ class Home extends StatelessWidget {
                         ),
                       ),
                       Text(
-                        'Gypsum Boards (1 kg) fgsfgs fd dfsg sdf g',
+                        'Cement',
                         style: TextStyle(
                           fontSize: 15,
                           fontWeight: FontWeight.bold,
                           color: Colors.black,
                         ),
                       ),
-                      SizedBox(height: 15),
                     ],
                   ),
                 ),
-              ),
-          ],
-        ),
-        const Text('data'),
-        const SizedBox(height: 20),
-      ],
+            ],
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              for (var i = 0; i < 3; i++)
+                Container(
+                  width: 100,
+                  // height: ,
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.blue[200],
+                  ),
+                  // color: Colors.red,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: const [
+                        SizedBox(height: 15),
+                        Text(
+                          '2500',
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          'Gypsum Boards (1 kg) fgsfgs fd dfsg sdf g',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                      ],
+                    ),
+                  ),
+                ),
+            ],
+          ),
+          const Text('data'),
+          const SizedBox(height: 20),
+        ],
+      ),
     );
   }
 }

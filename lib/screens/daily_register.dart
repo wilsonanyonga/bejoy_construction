dd26a3961fbac8f1dfda4ac2dd84420dff6a114a
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-05-06 22:00:15 +0300

    Update daily_register.dart

---
+++ lib/screens/daily_register.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DailyRegister extends StatelessWidget {
+  const DailyRegister({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Daily Register');
+  }
+}

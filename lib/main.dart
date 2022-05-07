a920d5a9362d616c71239b885f7af760348ee1cd
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-05-07 17:48:38 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,106 @@
 import 'package:bejoy_construction/screens/home.dart';
+import 'package:bejoy_construction/screens/material_use.dart';
+import 'package:bejoy_construction/screens/pictures.dart';
+import 'package:bejoy_construction/screens/receipts.dart';
+import 'package:bejoy_construction/screens/staff.dart';
+import 'package:bejoy_construction/screens/stock.dart';
+import 'package:bejoy_construction/widgets/menu_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  /// The title of the app.
+  static const String title = 'Bejoy Construction';
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Bejoy Construction',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+  Widget build(BuildContext context) => MaterialApp.router(
+        routeInformationParser: _router.routeInformationParser,
+        routerDelegate: _router.routerDelegate,
+        title: 'GoRouter Example',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.green,
+        ),
+      );
+
+  final GoRouter _router = GoRouter(
+    routes: <GoRoute>[
+      GoRoute(
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) =>
+            const MyHomePage(),
       ),
-      home: const MyHomePage(title: 'Bejoy Construction'),
-    );
-  }
+      GoRoute(
+        path: '/stock',
+        builder: (BuildContext context, GoRouterState state) => const Stock(),
+      ),
+      GoRoute(
+        path: '/staff',
+        builder: (BuildContext context, GoRouterState state) => const Staff(),
+      ),
+      GoRoute(
+        path: '/receipts',
+        builder: (BuildContext context, GoRouterState state) => const Receipt(),
+      ),
+      GoRoute(
+        path: '/daily_register',
+        builder: (BuildContext context, GoRouterState state) => const Receipt(),
+      ),
+      GoRoute(
+        path: '/tools',
+        builder: (BuildContext context, GoRouterState state) => const Receipt(),
+      ),
+      GoRoute(
+        path: '/pictures',
+        builder: (BuildContext context, GoRouterState state) =>
+            const Pictures(),
+      ),
+      GoRoute(
+        path: '/materials',
+        builder: (BuildContext context, GoRouterState state) =>
+            const MaterialUse(),
+      ),
+    ],
+  );
+
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     title: 'Bejoy Construction',
+  //     debugShowCheckedModeBanner: false,
+  //     theme: ThemeData(
+  //       // This is the theme of your application.
+  //       //
+  //       // Try running your application with "flutter run". You'll see the
+  //       // application has a blue toolbar. Then, without quitting the app, try
+  //       // changing the primarySwatch below to Colors.green and then invoke
+  //       // "hot reload" (press "r" in the console where you ran "flutter run",
+  //       // or simply save your changes to "hot reload" in a Flutter IDE).
+  //       // Notice that the counter didn't reset back to zero; the application
+  //       // is not restarted.
+  //       primarySwatch: Colors.green,
+  //     ),
+  //     home: const MyHomePage(title: 'Bejoy Construction'),
+  //   );
+  // }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  // const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -43,25 +111,19 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   final _coun2 = 1;
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+
+  const MyHomePage({Key? key}) : super(key: key);
+  // void _incrementCounter() {
+  //   setState(() {
+  //     // This call to setState tells the Flutter framework that something has
+  //     // changed in this State, which causes it to rerun the build method below
+  //     // so that the display can reflect the updated values. If we changed
+  //     // _counter without calling setState(), then the build method would not be
+  //     // called again, and so nothing would appear to happen.
+  //     _counter++;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -75,7 +137,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Center(child: Text(widget.title)),
+        title: const Center(child: Text(MyApp.title)),
         actions: [
           Builder(
             builder: (context) => IconButton(
@@ -117,15 +179,47 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: const Text('Bejoy Admin',
                     style:
                         TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
-            newMethodMenu(context, 'Home', Icons.home),
-            newMethodMenu(context, 'Stock', Icons.storage_rounded),
-            newMethodMenu(context, 'Staff', Icons.people),
-            newMethodMenu(context, 'Receipts', Icons.receipt_long_outlined),
-            newMethodMenu(context, 'Daily Register', Icons.app_registration),
-            newMethodMenu(context, 'Tools', Icons.settings_applications),
+            NewMethodMenuWidget(
+                context: context,
+                details: 'Home',
+                icon: Icons.home,
+                routeMe: '/'),
+            NewMethodMenuWidget(
+                context: context,
+                details: 'Stock',
+                icon: Icons.storage_rounded,
+                routeMe: '/stock'),
+            NewMethodMenuWidget(
+                context: context,
+                details: 'Staff',
+                icon: Icons.people,
+                routeMe: '/staff'),
+            NewMethodMenuWidget(
+                context: context,
+                details: 'Receipts',
+                icon: Icons.receipt_long_outlined,
+                routeMe: '/receipts'),
+            NewMethodMenuWidget(
+                context: context,
+                details: 'Daily Register',
+                icon: Icons.app_registration,
+                routeMe: '/daily_register'),
+            NewMethodMenuWidget(
+                context: context,
+                details: 'Tools',
+                icon: Icons.settings_applications,
+                routeMe: '/tools'),
+            NewMethodMenuWidget(
+                context: context,
+                details: 'Progress Pictures',
+                icon: Icons.picture_in_picture_alt,
+                routeMe: '/pictures'),
             // const Spacer(),
-            newMethodMenu(
-                context, 'Materials Use', Icons.settings_applications),
+            NewMethodMenuWidget(
+                context: context,
+                details: 'Materials Use',
+                icon: Icons.settings_applications,
+                routeMe: '/materials'),
           ],
         ),
       ),
@@ -141,42 +235,31 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               child: Text('Bejoy Projects'),
             ),
-            newMethodMenu(context, 'GLUK', Icons.home),
-            newMethodMenu(context, 'Oloitoktok', Icons.home),
-            newMethodMenu(context, 'Add Project', Icons.add_circle),
+            NewMethodMenuWidget(
+                context: context,
+                details: 'GLUK',
+                icon: Icons.home,
+                routeMe: '/stock'),
+            NewMethodMenuWidget(
+                context: context,
+                details: 'Oloitoktok',
+                icon: Icons.home,
+                routeMe: '/stock'),
+            NewMethodMenuWidget(
+                context: context,
+                details: 'Add Project',
+                icon: Icons.add_circle,
+                routeMe: '/stock'),
           ],
         ),
       ),
       // body: Home(_counter),
       body: Home(_coun2),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {},
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-
-  ListTile newMethodMenu(
-    BuildContext context,
-    String details,
-    IconData icon,
-  ) {
-    return ListTile(
-      leading: Icon(icon),
-      title: Text(
-        details,
-        style: const TextStyle(
-          fontSize: 20,
-          // fontWeight: FontWeight.bold,
-          color: Colors.black,
-        ),
-      ),
-      onTap: () {
-        // Update the state of the app.
-        // ...
-        Navigator.pop(context);
-      },
     );
   }
 }

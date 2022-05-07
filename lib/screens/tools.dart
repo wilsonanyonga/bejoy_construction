5d6f598bed1d128690723760b25fea25fd7ce2a6
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-05-07 17:48:29 +0300

    Update tools.dart

---
+++ lib/screens/tools.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Tools extends StatelessWidget {
+  const Tools({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Tools');
+  }
+}

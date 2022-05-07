0839e18084660703e9be7185a99237db55c9f42e
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-05-07 17:48:14 +0300

    Update staff.dart

---
+++ lib/screens/staff.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Staff extends StatelessWidget {
+  const Staff({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Staff');
+  }
+}

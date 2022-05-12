fac0681fe56cbe6dad6c431eff186f25df363feb
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-05-12 04:37:47 +0300

    Create data_one.g.dart

---
+++ lib/models/data_one.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'data_one.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Data _$DataFromJson(Map<String, dynamic> json) => Data(
+      id: json['id'] as int,
+      email: json['email'] as String,
+      firstName: json['first_name'] as String,
+      lastName: json['last_name'] as String,
+      avatar: json['avatar'] as String,
+    );
+
+Map<String, dynamic> _$DataToJson(Data instance) => <String, dynamic>{
+      'id': instance.id,
+      'email': instance.email,
+      'first_name': instance.firstName,
+      'last_name': instance.lastName,
+      'avatar': instance.avatar,
+    };

400688b129a12b2d0ccffe8e067620a1ec277706
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-05-22 23:12:20 +0300

    Create counter.g.dart

---
+++ lib/models/counter.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'counter.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CountLol _$CountLolFromJson(Map<String, dynamic> json) => CountLol(
+      count: json['count'] as int,
+    );
+
+Map<String, dynamic> _$CountLolToJson(CountLol instance) => <String, dynamic>{
+      'count': instance.count,
+    };

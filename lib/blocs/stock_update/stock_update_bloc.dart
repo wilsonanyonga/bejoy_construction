a45a7651d560a5afc0d7f770f9d8ae052639c9e1
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-05-26 21:55:01 +0300

    Create stock_update_bloc.dart

---
+++ lib/blocs/stock_update/stock_update_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'stock_update_event.dart';
+part 'stock_update_state.dart';
+
+class StockUpdateBloc extends Bloc<StockUpdateEvent, StockUpdateState> {
+  StockUpdateBloc() : super(StockUpdateInitial()) {
+    on<StockUpdateEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}

1e2326dc94d949a2de57db0a4127382301c0944f
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-06-22 20:37:49 +0300

    Create home_page.dart

---
+++ lib/models/home_page.dart
@@ -0,0 +1,48 @@
+import 'package:bejoy_construction/models/home_page/datum.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:equatable/equatable.dart';
+
+part 'home_page.g.dart';
+
+@JsonSerializable()
+// class Data {
+//   Data({
+//     required this.id,
+//     required this.project_id,
+//     required this.materials_name,
+//     required this.materials,
+
+//     "project_id": 1,
+//     "materials_name": "Cement",
+//     "created_at": "2022-05-03T00:00:00+03:00",
+//     "updated_at": "2022-05-21T00:00:00+03:00",
+//     "Materials": null
+//   });
+
+//   int id;
+//   String email;
+//   @JsonKey(name: 'first_name')
+//   String firstName;
+//   @JsonKey(name: 'last_name')
+//   String lastName;
+//   String avatar;
+
+//   factory Data.fromJson(Map<String, dynamic> json) => _$DataFromJson(json);
+
+//   Map<String, dynamic> toJson() => _$DataToJson(this);
+// }
+
+class HomePage {
+  HomePage({
+    required this.data,
+    required this.status,
+  });
+
+  List<Datum> data;
+  int status;
+
+  factory HomePage.fromJson(Map<String, dynamic> json) =>
+      _$HomePageFromJson(json);
+
+  Map<String, dynamic> toJson() => _$HomePageToJson(this);
+}

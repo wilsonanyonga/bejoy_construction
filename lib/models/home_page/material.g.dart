0c1e22b94d680a6e3cb5e376df001f28e7d029e0
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-06-23 15:03:45 +0300

    Update material.g.dart

--- lib/models/home_page/material.g.dart
+++ lib/models/home_page/material.g.dart
@@ -8,24 +8,22 @@ part of 'material.dart';
 
 Material _$MaterialFromJson(Map<String, dynamic> json) => Material(
       id: json['id'] as int,
-      createdAt: DateTime.parse(json['createdAt'] as String),
-      updatedAt: DateTime.parse(json['updatedAt'] as String),
-      deletedAt: json['deletedAt'],
-      materialId: json['materialId'] as int,
-      stockId: json['stockId'] as int,
+      createdAt: DateTime.parse(json['CreatedAt'] as String),
+      updatedAt: DateTime.parse(json['UpdatedAt'] as String),
+      deletedAt: json['DeletedAt'],
+      stockId: json['stock_id'] as int,
       amount: json['amount'] as int,
-      materialCreatedAt: DateTime.parse(json['materialCreatedAt'] as String),
-      materialUpdatedAt: DateTime.parse(json['materialUpdatedAt'] as String),
+      materialCreatedAt: DateTime.parse(json['created_at'] as String),
+      materialUpdatedAt: DateTime.parse(json['updated_at'] as String),
     );
 
 Map<String, dynamic> _$MaterialToJson(Material instance) => <String, dynamic>{
       'id': instance.id,
-      'createdAt': instance.createdAt.toIso8601String(),
-      'updatedAt': instance.updatedAt.toIso8601String(),
-      'deletedAt': instance.deletedAt,
-      'materialId': instance.materialId,
-      'stockId': instance.stockId,
+      'CreatedAt': instance.createdAt.toIso8601String(),
+      'UpdatedAt': instance.updatedAt.toIso8601String(),
+      'DeletedAt': instance.deletedAt,
+      'stock_id': instance.stockId,
       'amount': instance.amount,
-      'materialCreatedAt': instance.materialCreatedAt.toIso8601String(),
-      'materialUpdatedAt': instance.materialUpdatedAt.toIso8601String(),
+      'created_at': instance.materialCreatedAt.toIso8601String(),
+      'updated_at': instance.materialUpdatedAt.toIso8601String(),
     };

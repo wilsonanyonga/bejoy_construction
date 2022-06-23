3ee90161c43772d8684199d2e2da4486bd2ae593
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-06-23 15:03:42 +0300

    Update material.dart

--- lib/models/home_page/material.dart
+++ lib/models/home_page/material.dart
@@ -5,27 +5,34 @@ part 'material.g.dart';
 
 @JsonSerializable()
 class Material {
+  int id;
+  @JsonKey(name: 'CreatedAt')
+  DateTime createdAt;
+  @JsonKey(name: 'UpdatedAt')
+  DateTime updatedAt;
+  @JsonKey(name: 'DeletedAt')
+  dynamic deletedAt;
+  // @JsonKey(name: 'material_id')
+  // int materialId;
+  @JsonKey(name: 'stock_id')
+  int stockId;
+  int amount;
+  @JsonKey(name: 'created_at')
+  DateTime materialCreatedAt;
+  @JsonKey(name: 'updated_at')
+  DateTime materialUpdatedAt;
+
   Material({
     required this.id,
     required this.createdAt,
     required this.updatedAt,
     required this.deletedAt,
-    required this.materialId,
+    // required this.materialId,
     required this.stockId,
     required this.amount,
     required this.materialCreatedAt,
     required this.materialUpdatedAt,
   });
-
-  int id;
-  DateTime createdAt;
-  DateTime updatedAt;
-  dynamic deletedAt;
-  int materialId;
-  int stockId;
-  int amount;
-  DateTime materialCreatedAt;
-  DateTime materialUpdatedAt;
 
   factory Material.fromJson(Map<String, dynamic> json) =>
       _$MaterialFromJson(json);

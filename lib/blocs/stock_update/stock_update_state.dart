50522b26dad9d0abeb0a46637c02420d584db6b4
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-06-26 21:45:15 +0300

    Update stock_update_state.dart

--- lib/blocs/stock_update/stock_update_state.dart
+++ lib/blocs/stock_update/stock_update_state.dart
@@ -2,9 +2,23 @@ part of 'stock_update_bloc.dart';
 
 abstract class StockUpdateState extends Equatable {
   const StockUpdateState();
-  
+
   @override
   List<Object> get props => [];
 }
 
-class StockUpdateInitial extends StockUpdateState {}
+class StockUpdateInitial extends StockUpdateState {
+  // @override
+  // List<Object> get props => [];
+}
+
+class StockHomePageState extends StockUpdateState {
+  final String materialsName;
+  final List amount;
+  // var amount = [] ;
+
+  StockHomePageState(this.materialsName, this.amount);
+  @override
+  // TODO: implement props
+  List<Object> get props => [materialsName, amount];
+}

6c77c5934c2df8902624827fd2a124d1ad123b2d
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-06-27 22:53:03 +0300

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: unnecessary_const
 
+import 'package:bejoy_construction/blocs/stock_update/stock_update_bloc.dart';
 import 'package:bejoy_construction/utils/dio_client.dart';
 import 'package:flutter/material.dart';
 
@@ -21,139 +22,106 @@ class Home extends StatelessWidget {
     mediaQsize = MediaQuery.of(context).size;
     mediaQheight = mediaQsize.height;
     mediaQwidth = mediaQsize.width;
+    var stockAmount = 0;
 
     // final names = _client.getUser(id: '1');
-    _client.getUser(id: '1');
 
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(40.0, 0.0, 40.0, 0.0),
-            child: TextField(
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5),
-                  borderSide: BorderSide.none,
-                  // borderSide: const BorderSide(
-                  //   color: Colors.green,
-                  //   width: 1.0,
-                  // ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(
-                    color: Colors.blue,
-                    width: 1.0,
+    // _client.getUser(id: '1');
+
+    return BlocProvider(
+      create: (context) => StockUpdateBloc(
+        RepositoryProvider.of<DioClient>(context),
+      )..add(LoadApiEvent()),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(40.0, 0.0, 40.0, 0.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: BorderSide.none,
+                    // borderSide: const BorderSide(
+                    //   color: Colors.green,
+                    //   width: 1.0,
+                    // ),
                   ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                      color: Colors.blue,
+                      width: 1.0,
+                    ),
+                  ),
+                  filled: true,
+                  fillColor: Colors.blue.shade100,
+                  // input border should appear when data is being modified
+                  // border: OutlineInputBorder(
+                  //   borderRadius: BorderRadius.circular(10.0),
+                  // ),
+                  labelText: 'Search',
+                  prefixIcon: const Icon(Icons.search),
                 ),
-                filled: true,
-                fillColor: Colors.blue.shade100,
-                // input border should appear when data is being modified
-                // border: OutlineInputBorder(
-                //   borderRadius: BorderRadius.circular(10.0),
-                // ),
-                labelText: 'Search',
-                prefixIcon: const Icon(Icons.search),
+              ),
+
+              // child: TextField(
+              //   decoration: InputDecoration(
+              //     labelText: 'Gender',
+              //     fillColor: Colors.blue.shade100,
+              //     border: OutlineInputBorder(
+              //       borderRadius: BorderRadius.circular(30),
+              //     ),
+              //   ),
+              // ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Project Name: GLUK',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
               ),
             ),
-
-            // child: TextField(
-            //   decoration: InputDecoration(
-            //     labelText: 'Gender',
-            //     fillColor: Colors.blue.shade100,
-            //     border: OutlineInputBorder(
-            //       borderRadius: BorderRadius.circular(30),
-            //     ),
-            //   ),
-            // ),
-          ),
-          const SizedBox(height: 20),
-          const Text(
-            'Project Name: GLUK',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
+            const SizedBox(height: 20),
+            const Text(
+              'Project Cost: \$1,000,000',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
             ),
-          ),
-          const SizedBox(height: 20),
-          const Text(
-            'Project Cost: \$1,000,000',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
+            const SizedBox(height: 30),
+            const Text(
+              'Materials Bought',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
             ),
-          ),
-          const SizedBox(height: 30),
-          const Text(
-            'Materials Bought',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
-          ),
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              for (var i = 0; i < 3; i++)
-                Container(
-                  width: mediaQwidth * 0.3,
-                  // height: 100,
-                  // color: Colors.red,
-                  padding: const EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.red[400],
-                  ),
-                  child: Column(
-                    children: const [
-                      SizedBox(height: 15),
-                      Text(
-                        '1',
-                        style: TextStyle(
-                          fontSize: 40,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Text(
-                        'Cement',
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-            ],
-          ),
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              for (var i = 0; i < 3; i++)
-                Container(
-                  width: mediaQwidth * 0.3,
-                  // height: ,
-                  padding: const EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.blue[200],
-                  ),
-                  // color: Colors.red,
-                  child: SingleChildScrollView(
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                for (var i = 0; i < 3; i++)
+                  Container(
+                    width: mediaQwidth * 0.3,
+                    // height: 100,
+                    // color: Colors.red,
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.red[400],
+                    ),
                     child: Column(
                       children: const [
                         SizedBox(height: 15),
                         Text(
-                          '2500',
+                          '1',
                           style: TextStyle(
                             fontSize: 40,
                             fontWeight: FontWeight.bold,
@@ -161,23 +129,112 @@ class Home extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          'Gypsum Boards (1 kg) fgsfgs fd dfsg sdf g',
+                          'Cement',
                           style: TextStyle(
                             fontSize: 15,
                             fontWeight: FontWeight.bold,
                             color: Colors.black,
                           ),
                         ),
-                        SizedBox(height: 15),
                       ],
                     ),
                   ),
-                ),
-            ],
-          ),
-          const Text('data'),
-          const SizedBox(height: 20),
-        ],
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                for (var i = 0; i < 3; i++)
+                  Container(
+                    width: mediaQwidth * 0.3,
+                    // height: ,
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.blue[200],
+                    ),
+                    // color: Colors.red,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: const [
+                          SizedBox(height: 15),
+                          Text(
+                            '2500',
+                            style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Text(
+                            'Gypsum Boards (1 kg) fgsfgs fd dfsg sdf g',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 15),
+                        ],
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+            const Text('data'),
+            BlocBuilder<StockUpdateBloc, StockUpdateState>(
+                builder: (context, state) {
+              if (state is StockUpdateLoadingInitial) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              if (state is StockHomePageState) {
+                return Column(
+                  children: [
+                    Text(state.status.toString()),
+                    Text(state.data.length.toString()),
+                    SizedBox(
+                      height: 110,
+                      child: ListView.builder(
+                          itemCount: state.data.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return ListTile(
+                              leading: const Icon(Icons.abc_outlined),
+                              // ${state.data[index].materials[index].amount}
+                              title: Column(
+                                children: [
+                                  Text(
+                                      "list is ${state.data[index].materialsName} sum is "),
+                                  // for (var i = 0; i < state.data[index].materials.length; i++)
+                                  //   stockAmount += state.data[index].materials[i].amount,
+
+                                  for (var i = 0;
+                                      i < state.data[index].materials.length;
+                                      i++)
+                                    Text(
+                                        "${state.data[index].materials[i].amount}")
+                                ],
+                              ),
+                            );
+                          }),
+                    ),
+                    // for (var i = 0; i < state.data.length; i++)
+                    //   Text(state.data[i].materialsName)
+                    //   for (var j = 0; j < state.data[i].materials.length; j++)
+
+                    // for (var j = 0; j < state.data[i].materials.length; j++) {
+                    //   Text(state.data[i].materials[j]);
+                    // }
+                  ],
+                );
+              }
+              return const Text('data did not work');
+            }),
+            const SizedBox(height: 20),
+          ],
+        ),
       ),
     );
   }

66020fab9adb995b454a559f76728d810d51f112
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-07-13 16:24:13 +0200

    Update stock_update_bloc.dart

--- lib/blocs/stock_update/stock_update_bloc.dart
+++ lib/blocs/stock_update/stock_update_bloc.dart
@@ -1,14 +1,18 @@
 // import 'dart:html';
 
+import 'package:bejoy_construction/models/hive/project_display.dart';
 import 'package:bejoy_construction/models/home_page.dart';
 import 'package:bejoy_construction/models/home_page/datum.dart';
 import 'package:bejoy_construction/utils/dio_client.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 part 'stock_update_event.dart';
 part 'stock_update_state.dart';
+
+late Box box;
 
 class StockUpdateBloc extends Bloc<StockUpdateEvent, StockUpdateState> {
   final DioClient _client = DioClient();
@@ -18,6 +22,11 @@ class StockUpdateBloc extends Bloc<StockUpdateEvent, StockUpdateState> {
       // TODO: implement event handler
     });
     on<LoadApiEvent>((event, emit) async {
+      ProjectDisplay projId2 = box.get('mainProject');
+      if (kDebugMode) {
+        print("project lol is done2");
+        print(projId2.myId);
+      }
       final activity = await _client.getUser(id: '1');
       // mat = activity.data[materialsName];
       // var total = 0;
@@ -30,6 +39,19 @@ class StockUpdateBloc extends Bloc<StockUpdateEvent, StockUpdateState> {
       //   print(total);
       // }
       emit(StockHomePageState(activity!.data, activity.status));
+    });
+    on<LoadHiveInitial>((event, emit) async {
+      await Hive.initFlutter();
+
+      box = await Hive.openBox('box');
+
+      Hive.registerAdapter(ProjectDisplayAdapter());
+      box.put('mainProject', ProjectDisplay(myId: 1));
+      ProjectDisplay projId2 = box.get('mainProject');
+      if (kDebugMode) {
+        print("lol is done");
+        print(projId2.myId);
+      }
     });
   }
 }

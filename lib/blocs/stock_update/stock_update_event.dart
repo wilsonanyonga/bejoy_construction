8f6c030d3b7c54dd28564bfe89aafecaed985856
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-07-13 16:24:19 +0200

    Update stock_update_event.dart

--- lib/blocs/stock_update/stock_update_event.dart
+++ lib/blocs/stock_update/stock_update_event.dart
@@ -2,15 +2,22 @@ part of 'stock_update_bloc.dart';
 
 abstract class StockUpdateEvent extends Equatable {
   const StockUpdateEvent();
+  @override
+  List<Object> get props => [];
 }
 
 class LoadApiEvent extends StockUpdateEvent {
-  @override
-  List<Object> get props => [];
+  // @override
+  // List<Object> get props => [];
 
   // @override
   // ignore: todo
   // // TODO: implement props
   // List<Object?> get props => throw UnimplementedError();
 
+}
+
+class LoadHiveInitial extends StockUpdateEvent {
+  // @override
+  // List<Object> get props => [];
 }

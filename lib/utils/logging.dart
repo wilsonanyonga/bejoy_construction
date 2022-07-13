5294d123942ebbe9d90fe04d00a35d6a12d06e9f
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-07-14 01:27:58 +0300

    Update logging.dart

--- lib/utils/logging.dart
+++ lib/utils/logging.dart
@@ -1,25 +1,32 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 
 class Logging extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    print('REQUEST[${options.method}] => PATH: ${options.path}');
+    if (kDebugMode) {
+      print('REQUEST[${options.method}] => PATH: ${options.path}');
+    }
     return super.onRequest(options, handler);
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    print(
-      'RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}',
-    );
+    if (kDebugMode) {
+      print(
+        'RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}',
+      );
+    }
     return super.onResponse(response, handler);
   }
 
   @override
   void onError(DioError err, ErrorInterceptorHandler handler) {
-    print(
-      'ERROR[${err.response?.statusCode}] => PATH: ${err.requestOptions.path}',
-    );
+    if (kDebugMode) {
+      print(
+        'ERROR[${err.response?.statusCode}] => PATH: ${err.requestOptions.path}',
+      );
+    }
     return super.onError(err, handler);
   }
 }
